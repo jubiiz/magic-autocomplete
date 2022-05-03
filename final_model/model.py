db434fb960523bc406eb085a618dc45735b66bd6
Julien Audet <julien.audet@hoptech.ca>
2022-05-03 10:16:55 -0400

    WIP added warmup and call to model

--- final_model/model.py
+++ final_model/model.py
@@ -9,11 +9,34 @@ class FullARModel(tf.keras.Model):
         self.units = 128
         self.embedding_dim = 64
         self.embedding = Embedding(input_dim=VOCAB_SIZE, output_dim=self.embedding_dim, mask_zero=True)
-        self.lstm = LSTM(self.units)
+        self.lstm = LSTM(self.units, return_state=True)
         self.lstm_cell = LSTMCell(self.units)
         self.dense = Dense(self.units, activation='relu')
         self.softmax = Dense(VOCAB_SIZE, activation='softmax')
 
-    def __call__(self):
-        pass
+    def warmup(self, inputs):
+        # taken from https://www.tensorflow.org/tutorials/structured_data/time_series#advanced_autoregressive_model
+        # inputs.shape => (batch, time, features)
+        # x.shape => (batch, lstm_units)
+        embedded = self.embedding(inputs, mask_zero=True)
+        mask = embedded._keras_mask
+        tf.print(mask)
+        x, *state = self.lstm_rnn(embedded)
+        # predictions.shape => (batch, vocab_size)
+        prediction = self.deep_layers(x)
+        return prediction, state
+
+    def deep_layers(self, x):
+        deep = self.dense(x)
+        prediction = self.softmax(deep)
+        return prediction
+
+    def call(self, inputs, training=None):
+        predictions = []
+
+        prediction, states = self.warmup(inputs)
+
+        predictions.append(prediction)
+
+        for i in
 
