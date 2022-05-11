8912ffb33f1a2a00dd249013200a6cf5aa9c0a94
Julien Audet <julien.audet@hoptech.ca>
2022-05-11 01:25:05 -0400

    WIP

--- final_model/run_final_model.py
+++ final_model/run_final_model.py
@@ -1,3 +1,6 @@
+import os
+import argparse
+
 from metadata import F_SINGLES_PATH, UNF_SINGLES_PATH, F_LISTS_DIR, UNF_LISTS_DIR
 import numpy as np
 import tensorflow as tf
@@ -6,6 +9,8 @@ from model import FullARModel, compile_and_fit
 
 
 def run():
+    args = get_args()
+
     decklists = load_decklists()
     raw_train, raw_test, raw_val = train_test_val_split(decklists)
 
@@ -22,6 +27,48 @@ def run():
     print("saved")
 
 
+def get_args():
+    args_parser = argparse.ArgumentParser()
+
+    args_parser.add_argument(
+        '--batch-size',
+        default=64,
+        type=int,
+    )
+    args_parser.add_argument(
+        '--num-epochs',
+        default=5,
+        type=int,
+    )
+    args_parser.add_argument(
+        '--num-units',
+        default=128,
+        type=int,
+    )
+    args_parser.add_argument(
+        '--hp-tune',
+        default=False,
+        type=bool,
+    )
+    args_parser.add_argument(
+        '--extra-dense',
+        help='True: add an extra dense layer in the model, default is false',
+        default=False,
+        type=bool,
+    )
+    # Saved model arguments
+    args_parser.add_argument(
+        '--job-dir',
+        default=os.getenv('AIP_MODEL_DIR'),
+        help='GCS location to export models')
+    args_parser.add_argument(
+        '--model-name',
+        default="ht-magic-autocomplete",
+        help='The name of your saved model')
+
+    return args_parser.parse_args()
+
+
 if __name__ == "__main__":
-    #tf.config.run_functions_eagerly(True)
+    # tf.config.run_functions_eagerly(True)
     run()
